7c81adeae038ef9fc238b4c7ec980158e0a0872b
ckim812 <109961170+ckim812@users.noreply.github.com>
2023-01-10 16:00:56 -0600

    working on routing and login/logout function

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,21 +1,21 @@
 const router = require("express").Router();
-const { User } = require('../models');
-const withAuth = require('../utils/auth');
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
 
-// router.get('/', (req, res) => {
-//   res.render('homepage');
-// });
+router.get("/", (req, res) => {
+  res.render("homepage");
+});
 
-router.get('/', withAuth, async (req, res) => {
+router.get("/", withAuth, async (req, res) => {
   try {
     const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
+      attributes: { exclude: ["password"] },
+      order: [["name", "ASC"]],
     });
 
     const users = userData.map((project) => project.get({ plain: true }));
 
-    res.render('homepage', {
+    res.render("homepage", {
       users,
       logged_in: req.session.logged_in,
     });
@@ -24,18 +24,21 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
 router.get("/signup", (req, res) => {
-  
-    res.render("signup");
+  res.render("signup");
+});
+
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard");
 });
 
 module.exports = router;
